8feb5e6848db7d9a4a322b9daeca73eeb4ee5175
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2023-12-04 14:04:29 +0100

    add day02

---
+++ day02.py
@@ -0,0 +1,44 @@
+from aocd import get_data
+import time
+import re
+
+
+def part1(data):
+    color = {"red": 12, "green": 13, "blue": 14}
+    res = 0
+    for i, line in enumerate(data.split("\n")):
+        valid = True
+        for pair in re.findall(r"\d+\s(?:green|red|blue)", line):
+            cnt, col = pair.split()
+            if int(cnt) > color[col]:
+                valid = False
+                break
+        if valid:
+            res += i + 1
+    return res
+
+
+def part2(data):
+    res = 0
+    for line in data.split("\n"):
+        color = {"red": 0, "green": 0, "blue": 0}
+        for pair in re.findall(r"\d+\s(?:green|red|blue)", line):
+            cnt, col = pair.split()
+            color[col] = max(int(cnt), color[col])
+        res += color["red"] * color["green"] * color["blue"]
+    return res
+
+
+if __name__ == "__main__":
+    data = get_data(day=2, year=2023)
+    t1 = time.time()
+    res1 = part1(data)
+    t2 = time.time()
+    res2 = part2(data)
+    t3 = time.time()
+    print("----------------Part 1-----------------")
+    print(f"time: {t2-t1}")
+    print(res1)
+    print("----------------Part 2-----------------")
+    print(f"time: {t3-t2}")
+    print(res2)
