fe2e4618861399e6ad2a1429b4e53e9031afb221
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2023-12-09 09:54:20 +0100

    add correct combinator

--- day09.hs
+++ day09.hs
@@ -1,7 +1,8 @@
 main = readFile "day09.txt" >>= print . getResult . map (map solve . splitParts . parseLine) . lines
     where
+        violet_starling = (\a b c -> a (b c) c)
         parseLine = (pure . map (read :: String -> Int)) . words
-        splitParts = (++) <*> map reverse
+        splitParts = (++) `violet_starling` map reverse
         getResult = foldl1 (zipWith (+))
         solve xs = if any (/=0) xs 
                   then last xs - solve ((zipWith (-) <*> tail) xs) 
