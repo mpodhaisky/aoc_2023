9956f11c3b48506d59b82399ca29325e99513b19
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2023-12-09 07:33:17 +0100

    add day 9

---
+++ day09.py
@@ -0,0 +1,48 @@
+from aocd import get_data
+import time
+
+
+def part1(data):
+    res = 0
+    for line in data.split("\n"):
+        line = list(map(int, line.split()))[::-1]
+        cnt = line[0]
+        while set(line) != {0}:
+            for i in range(len(line) - 1):
+                line[i] = line[i] - line[i + 1]
+            line.pop()
+            cnt += line[0]
+        res += cnt
+    return res
+
+
+def part2(data):
+    res = 0
+    for line in data.split("\n"):
+        line = list(map(int, line.split()))[::-1]
+        cnt, sgn = line[-1], -1
+        while set(line) != {0}:
+            for i in range(len(line) - 1):
+                line[i] = line[i] - line[i + 1]
+            line.pop()
+            cnt += sgn * line[-1]
+            sgn *= -1
+        res += cnt
+    return res
+
+
+if __name__ == "__main__":
+    day, year = 9, 2023
+    data = get_data(day=day, year=year)
+    t1 = time.time()
+    res1 = part1(data)
+    t2 = time.time()
+    res2 = part2(data)
+    t3 = time.time()
+    print(f"----------------day {str(day).zfill(2)}-----------------")
+    print("----------------Part 1-----------------")
+    print(f"time: {t2-t1}")
+    print(f"flag: {res1}")
+    print("----------------Part 2-----------------")
+    print(f"time: {t3-t2}")
+    print(f"flag: {res2}")
