2640d720cc2fe994274360b28fd16e051ef62c2a
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2023-12-11 15:29:28 +0100

    refactor day 11

--- day11.py
+++ day11.py
@@ -4,50 +4,34 @@ from aocd import get_data
 import time
 
 
-def part1(data):
+def solve(data, gap):
     matrix = list(map(list, data.split("\n")))
+    m, n = len(matrix), len(matrix[0])
     galaxies = []
-    row, col = [0] * len(matrix), [0] * len(matrix[0])
-    cnt = 0
-    for i, line in enumerate(matrix):
-        if "#" not in line:
-            cnt += 1000000 - 1
-        row[i] = cnt
-    cnt = 0
-    for i, line in enumerate(zip(*matrix)):
-        if "#" not in line:
-            cnt += 1000000 - 1
-        col[i] = cnt
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
+    row, col = [gap - 1] * m, [gap - 1] * n
+    for i in range(m):
+        for j in range(n):
             if matrix[i][j] == "#":
+                row[i] = col[j] = 0
                 galaxies.append((i, j))
+    for i in range(1, m):
+        row[i] += row[i - 1]
+    for i in range(1, n):
+        col[i] += col[i - 1]
     res = 0
-    for i in range(len(galaxies)):
-        for j in range(i + 1, len(galaxies)):
-            res += abs(
-                galaxies[j][0]
-                + row[galaxies[j][0]]
-                - (galaxies[i][0] + row[galaxies[i][0]])
-            ) + abs(
-                galaxies[j][1]
-                + col[galaxies[j][1]]
-                - (galaxies[i][1] + col[galaxies[i][1]])
-            )
-    print(res)
-
-
-def part2(data):
-    pass
+    for i, (a, b) in enumerate(galaxies):
+        for c, d in galaxies[i + 1 :]:
+            res += abs(row[a] + a - (row[c] + c)) + abs(col[b] + b - (col[d] + d))
+    return res
 
 
 if __name__ == "__main__":
     day, year = 11, 2023
     data = get_data(day=day, year=year)
     t1 = time.time()
-    res1 = part1(data)
+    res1 = solve(data, 2)
     t2 = time.time()
-    res2 = part2(data)
+    res2 = solve(data, 1000000)
     t3 = time.time()
     print(f"----------------day {str(day).zfill(2)}-----------------")
     print("----------------Part 1-----------------")
