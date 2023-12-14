eb1f394f779e30ab5c64392d24f411f55ddb5cc0
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2023-12-14 19:56:40 +0100

    add super slow asymptotically optimal day 14 part 2

--- day14.py
+++ day14.py
@@ -15,24 +15,36 @@ def part1(data):
     return t
 
 
+def cycle(grid):
+    for _ in range(4):
+        grid = list(map(lambda x: "".join(x), zip(*grid)))
+        grid = [
+            "#".join(
+                ["".join(sorted(group, reverse=True)) for group in line.split("#")]
+            )
+            for line in grid
+        ]
+        grid = list(map(lambda x: x[::-1], grid))
+    return grid
+
+
 def part2(data):
     grid = data.strip().split("\n")
-    new_grid = []
-    seen = []
-    while grid not in seen:
-        seen.append(grid)
-        for _ in range(4):
-            for line in zip(*grid):
-                line = "".join(line)
-                groups = line.split("#")
-                new_grid.append(
-                    "#".join(["".join(sorted(group)[::-1]) for group in groups])
-                )
-            grid, new_grid = list(map(lambda x: x[::-1], new_grid)), []
-    cycle_start = seen.index(grid)
-    goal = 1000000000 - cycle_start
-    seen = seen[cycle_start:]
-    grid = seen[goal % len(seen)]
+    slow, fast = cycle(grid), cycle(cycle(grid))
+    while slow != fast:
+        slow, fast = cycle(slow), cycle(cycle(fast))
+    cycle_start = 0
+    slow = grid
+    while slow != fast:
+        slow, fast = cycle(slow), cycle(fast)
+        cycle_start += 1
+    cycle_length = 1
+    fast = cycle(slow)
+    while slow != fast:
+        fast = cycle(fast)
+        cycle_length += 1
+    for _ in range((1000000000 - cycle_start) % cycle_length + cycle_start):
+        grid = cycle(grid)
     t = 0
     for i, l in enumerate(grid[::-1], 1):
         t += l.count("O") * i
